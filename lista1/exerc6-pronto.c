646ccf88d81c051b4f7183fd52c079e6428be7bd
Frederico <fredericolarangeira@hotmail.com>
2022-04-30 17:42:09 -0300

    exerc6 lista 1 pronto

--- lista1/exerc6-pronto.c
+++ lista1/exerc6-pronto.c
@@ -1,25 +1,33 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <time.h>
 
 void aloca_vetor(int *vetor, int tamanho){
     int i;
 
-    vetor = realloc(vetor, tamanho * sizeof(int));
+    vetor = (int *) realloc(vetor, tamanho * sizeof(int));
 
     for (i = 0; i < tamanho - 1; i++){
-        
+        vetor[i] = rand();
     }
-    
+
+    return;
 }
 
 int main(){
     int *vetor = (int *) malloc(1 * sizeof(int));
-    int tamanho;
+    int tamanho, i;
+
+    srand(time(NULL));
 
     printf("Insira o tamanho do vetor: ");
-    scanf("%d", tamanho);
+    scanf("%d", &tamanho);
 
     aloca_vetor(vetor, tamanho);
+
+    for (i = 0; i < tamanho; i++){
+        printf("%d\n", vetor[i]);
+    }
 
     free(vetor);
 
